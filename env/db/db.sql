e1acb94ab3b33205666d3557011fabeb37c253b4
Mark <markusx64bit@inbox.ru>
2020-04-05 12:24:47 +0800

    update db.sql

--- env/db/db.sql
+++ env/db/db.sql
@@ -1,5 +1,3 @@
--- Версия сервера: 10.4.11-MariaDB
-
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -12,8 +10,33 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE DATABASE IF NOT EXISTS `t` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE `t`;
+--
+-- База данных: `t2`
+--
+CREATE DATABASE IF NOT EXISTS `t2` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `t2`;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `task`
+--
+
+CREATE TABLE `task` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `performed` tinyint(1) NOT NULL DEFAULT 0,
+  `date_create` date NOT NULL DEFAULT curdate(),
+  `updated` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
@@ -28,10 +51,43 @@ CREATE TABLE `user` (
   `date_birthday` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Дамп данных таблицы `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `password_hash`, `country`, `avatar`, `date_create`, `description`, `date_update`, `date_birthday`) VALUES
+(3, 'admin', 'admin@admin.com', '$2y$10$SGb2IDKI0WeVCEn7VPKf1ul8MpziHvqLAyA9sjXqamEhr920Bc0m2', '0', NULL, '2020-04-05', '                                    ', NULL, '2020-04-05');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `user`
+--
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email_UNIQUE` (`email`);
 
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `task`
+--
+ALTER TABLE `task`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `user`
+--
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 SET FOREIGN_KEY_CHECKS=1;
